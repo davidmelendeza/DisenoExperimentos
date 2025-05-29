3f6bae4df6d0c0f072e3e51e44ffacbb9d678a4c
davidmelendeza <70189628+davidmelendeza@users.noreply.github.com>
2025-05-29 00:49:00 -0600

    Codigo 1-11

--- Lab5/Lab.R
+++ Lab5/Lab.R
@@ -11,5 +11,60 @@ IDE$Experiencia <- factor (IDE$Experiencia,
 
 str(IDE) 
 
+#1
 table(IDE$Herramienta, IDE$Experiencia) 
 
+#2
+group_by(IDE, Experiencia) %>%
+  summarise(
+    count = n(),
+    mean = mean(Duracion, na.rm = TRUE),
+    var = var(Duracion, na.rm = TRUE),
+    sd = sd(Duracion, na.rm = TRUE)
+  )
+
+#3
+group_by(IDE, Herramienta, Experiencia) %>%
+  summarise(
+    count = n(),
+    mean = mean(Duracion, na.rm = TRUE),
+    var = var(Duracion, na.rm = TRUE),
+    sd = sd(Duracion, na.rm = TRUE)
+  )
+
+boxplot(Duracion ~ Herramienta, data=IDE, frame = FALSE, col = c("#00AFBB", "#E7B800"), ylab=" Duracion")
+
+#4 y 5 tiene que hacerlas xd
+
+# 6
+boxplot(Duracion ~ Herramienta * Experiencia, data=IDE, frame = FALSE, col = c("#00AFBB", "#E7B800"), ylab="Duracion")
+        
+boxplot(Duracion ~ Experiencia * Herramienta, data=IDE, frame = FALSE, col = c("#00AFBB", "#E7B800"), ylab="Duracion")
+        
+# 7
+
+IDE %>%
+  ggplot() +
+  aes(x = Herramienta, color = Experiencia, group = Experiencia, y = Duracion) +
+  stat_summary(fun = mean, geom = "point") +
+  stat_summary(fun = mean, geom = "line")
+
+#8 hay que hacerla
+
+#9
+interaction.plot(x.factor = IDE$Experiencia, trace.factor = IDE$Herramienta,
+                 response = IDE$Duracion, fun = mean,
+                 type = "b", legend = TRUE,
+                 xlab = "Experiencia", ylab="Duración en horas",
+                 pch=c(1,19), col = c("#00AFBB", "#E7B800"))
+
+# 10
+res.aov <- aov(Duracion ~ Herramienta + Experiencia, data = IDE)
+summary(res.aov)
+
+#11
+res.aov_inter <- aov(Duracion ~ Herramienta * Experiencia, data = IDE)
+summary(res.aov_inter)
+
+res.aov_inter2 <- aov(Duracion ~ Herramienta + Experiencia + Herramienta:Experiencia, data = IDE)
+summary(res.aov_inter2)
